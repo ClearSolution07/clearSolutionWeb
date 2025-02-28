ff736a14eb3828cc7449924b1e14823636b2dc03
Jitam Bhardawaj <93033828+bharadwajjitam101@users.noreply.github.com>
2025-03-01 01:02:28 +0530

    Update Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -13,28 +13,28 @@ const Products = () => {
     {
       title: "Cura Tech Engineering Portfolio",
       description: "Cura Tech Engineering provides essential equipment and builds custom manufacturing for hospitals and other industries. The company offers end-to-end solutions, including medical equipment, hospital furnishings, and advanced technological systems. An innovative website for this growing company, with modern and responsive infrastructure development. Clear Solution Services developed Cura Tech Engineering's portfolio website, creating a professional and user-friendly digital presence. The website highlights the company's expertise, achievements, and commitment to technological excellence and innovation.",
-      image: "https://cloud.appwrite.io/v1/storage/buckets/67c0adbf0010a0a0c361/files/67c0b88f002a5aea6e27/view?project=67c0ad3100142add304a&mode=admin",
+      image: "public/curawebdesign-3.gif",
       align: "right",
       url: "https://curatechengineering.com"
     },
     {
       title: "Saarathi Website",
       description: "Saarathi (Guide) is an innovative e-learning platform that aims to reach out to core learners, helping them enhance access opportunities, and address concerns at a national level. Hosted by Azim M Ajmal, in partnership with UNICEF-India, it is supported by civil society organizations who have shown their commitment to education. Core learners are individuals who have spent at least three years or less, based on circumstances. In a child's first institution, the school, we aim to create a safe space for them and their families, welcoming all, regardless of background. Clear Solution Services developed Saarathi's official website, ensuring a professional and accessible platform.",
-      image: "https://cloud.appwrite.io/v1/storage/buckets/67c0adbf0010a0a0c361/files/67c0bcce0001776ead1d/view?project=67c0ad3100142add304a&mode=admin",
+      image: "public/saarthiwebdesign.gif",
       align: "left",
       url: "https://saarathi.example.com"
     },
     {
       title: "Focus Mobile App",
       description: "Focus Mobile App is a comprehensive study resource that helps users prepare for exams, stay updated with current affairs, and access valuable study resources. It provides a user-friendly interface for practicing tests, browsing the latest news, and enhancing knowledge across various subjects. The app is designed with a modern approach, offering a seamless experience with intuitive navigation, real-time updates, and innovative features to support students and professionals in their learning journey.",
-      image: "https://cloud.appwrite.io/v1/storage/buckets/67c0adbf0010a0a0c361/files/67c0adf6002e3b6e1a17/view?project=67c0ad3100142add304a&mode=admin",
+      image: "public/focuswebdesign.gif",
       align: "right",
       url: "https://focusapp.example.com"
     },
     {
       title: "CV Maker",
       description: "CV Maker is a free, user-friendly SaaS product designed for creating CVs / Resumes. It features a simple interface, professional templates, and advanced customization options. Users craft personalized resumes easily. The tool is lightweight, indicating fast performance, and was developed using modern web technologies. The team at Clear Solution Services aims to address a new market need by offering an accessible solution for CV creation without requiring technical expertise.",
-      image: "https://cloud.appwrite.io/v1/storage/buckets/67c0adbf0010a0a0c361/files/67c0b70000383481ad9a/view?project=67c0ad3100142add304a&mode=admin",
+      image: "public/cvmaker.dd44350f.gif",
       align: "left",
       url: "https://cvmaker.example.com"
     }
